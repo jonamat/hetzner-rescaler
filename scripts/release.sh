6466e2955506c5157f10858ac28e352ae65ec0a0
Jonathan Mataloni <jo.mataloni@gmail.com>
2021-11-17 22:37:10 +0100

    Add ARMv7 support, licence and readme to releases

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,12 +10,20 @@ rm -rf ./release
 mkdir ./release/ \
 ./release/${PREFIX}windows-amd64/ \
 ./release/${PREFIX}linux-amd64/ \
+./release/${PREFIX}linux-arm/ \
 ./release/${PREFIX}linux-arm64/
+
+# Copy assets
+cp -R ./LICENCE ./README.md ./CHANGELOG.md ./release/${PREFIX}windows-amd64/ \
+cp -R ./LICENCE ./README.md ./CHANGELOG.md ./release/${PREFIX}linux-amd64/ \
+cp -R ./LICENCE ./README.md ./CHANGELOG.md ./release/${PREFIX}linux-arm/ \
+cp -R ./LICENCE ./README.md ./CHANGELOG.md ./release/${PREFIX}linux-arm64/
 
 # Build for each platform
 GOOS=windows GOARCH=amd64 go build -o ./release/${PREFIX}windows-amd64/hetzner-rescaler.exe . &
 GOOS=linux GOARCH=amd64 go build -o ./release/${PREFIX}linux-amd64/hetzner-rescaler . &
 GOOS=linux GOARCH=arm64 go build -o ./release/${PREFIX}linux-arm64/hetzner-rescaler . &
+GOOS=linux GOARCH=arm go build -o ./release/${PREFIX}linux-arm/hetzner-rescaler . &
 wait
 
 # Zip folders
@@ -23,10 +31,12 @@ cd ./release/
 zip -r ./${PREFIX}windows-amd64.zip ./${PREFIX}windows-amd64/ &
 zip -r ./${PREFIX}linux-amd64.zip ./${PREFIX}linux-amd64/ &
 zip -r ./${PREFIX}linux-arm64.zip ./${PREFIX}linux-arm64/ &
+zip -r ./${PREFIX}linux-arm.zip ./${PREFIX}linux-arm/ &
 wait
 
 # Destroy temp dirs
 rm -rf ./${PREFIX}windows-amd64 &
 rm -rf ./${PREFIX}linux-amd64 &
 rm -rf ./${PREFIX}linux-arm64 &
+rm -rf ./${PREFIX}linux-arm &
 wait
